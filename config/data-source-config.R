6df4b7071b9f46cde3c0115b8fb38048a144175e
Patrick <19709369+ocelot-engineering@users.noreply.github.com>
2023-03-05 17:48:16 +0000

    Rename files to follow consistent pattern of dash separated rather than underscore

--- config/data-source-config.R
+++ config/data-source-config.R
@@ -16,7 +16,7 @@ output_dir <- proj_config$data_path
 
 daily_cases = list(
     url = paste0(base_url, 'WHO-COVID-19-global-data.csv')
-  , output_path = paste0(output_dir, 'daily_cases.rds')
+  , output_path = paste0(output_dir, 'daily-cases.rds')
   , col_types = readr::cols(
       Date_reported     = readr::col_date(format = '%Y-%m-%d')
     , Country_code      = readr::col_character()
@@ -37,7 +37,7 @@ vaccination = list(
     , ISO3                                 = readr::col_character()
     , WHO_REGION                           = readr::col_character()
     , DATA_SOURCE                          = readr::col_character()
-    , DATE_UPDATED                         = readr::col_date(format = '%Y-%m-%d')
+    , DATE_UPDATED                         = readr::col_date(format = "%Y-%m-%d")
     , TOTAL_VACCINATIONS                   = readr::col_double()  # some rows larger than .Machine$integer.max (2147483647). Use double instead
     , PERSONS_VACCINATED_1PLUS_DOSE        = readr::col_integer() # source stated double but data shows otherwise
     , TOTAL_VACCINATIONS_PER100            = readr::col_double()  # source stated integer but data shows otherwise
@@ -45,10 +45,10 @@ vaccination = list(
     , PERSONS_FULLY_VACCINATED             = readr::col_integer()
     , PERSONS_FULLY_VACCINATED_PER100      = readr::col_double()
     , VACCINES_USED                        = readr::col_character()
-    , FIRST_VACCINE_DATE                   = readr::col_date(format = '%Y-%m-%d')
+    , FIRST_VACCINE_DATE                   = readr::col_date(format = "%Y-%m-%d")
     , NUMBER_VACCINES_TYPES_USED           = readr::col_integer()
     , PERSONS_BOOSTER_ADD_DOSE             = readr::col_integer()
-    , PERSONS_BOOSTER_ADD_DOSE_PER100      = readr::col_double() 
+    , PERSONS_BOOSTER_ADD_DOSE_PER100      = readr::col_double()
     )
 )
 
